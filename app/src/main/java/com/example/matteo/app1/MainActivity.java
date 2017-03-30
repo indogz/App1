c3677d6626513e51dbccb96eeb42598cfb673bdd
indogz <lalavegna@gmail.com>
2017-03-30 11:32:08 +0200

    3

--- app/src/main/java/com/example/matteo/app1/MainActivity.java
+++ app/src/main/java/com/example/matteo/app1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.matteo.app1;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -11,27 +14,39 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import static com.example.matteo.app1.MyFirebaseInstaceService.REG_TOKEN;
+public class MainActivity extends AppCompatActivity implements ValueEventListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
+    private TextView headingText;
+    private EditText headingInput;
+    private RadioButton rbRed, rbBlue;
     //prendamo un'istanza
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     //prendaimo la root
-    protected DatabaseReference rootReference = firebaseDatabase.getReference();
+    protected DatabaseReference mRootRef = firebaseDatabase.getReference();
     //child node
-    private DatabaseReference childReference = rootReference.child("message");
+    private DatabaseReference childReference = mRootRef.child("heading");
+    private DatabaseReference childRef = mRootRef.child("key");
+    private DatabaseReference mHeadingReference = mRootRef.child("heading");
+    private DatabaseReference mFontColorRef = mRootRef.child("fontColor");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView= (TextView) findViewById(R.id.msgTxt);
-        textView.setText("Message appear here");
+
+        setGraphicItems();
+
+        headingText.setText("Message appear here");
 
 
+    }
 
+    private void setGraphicItems() {
+        headingInput = (EditText) findViewById(R.id.headingInput);
+        headingText = (TextView) findViewById(R.id.msgTxt);
+        rbRed = (RadioButton) findViewById(R.id.rbRed);
+        rbBlue = (RadioButton) findViewById(R.id.rBlue);
 
     }
 
@@ -39,12 +54,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         //listener del cambio data
-        childReference.addValueEventListener(new ValueEventListener() {
+       /* childReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String message = dataSnapshot.getValue(String.class);
                 //display data
-                textView.setText(message);
+                headingText.setText(message);
 
             }
 
@@ -52,7 +67,59 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });
+        });*/
+
+        mHeadingReference.addValueEventListener(this);
+        mFontColorRef.addValueEventListener(this);
+    }
+
+    public void submitHeading(View view) {
+        String heading = headingInput.getText().toString();
+        mHeadingReference.setValue(heading);
+        headingInput.setText("");
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.rbRed:
+                mFontColorRef.setValue("red");
+                break;
+            case R.id.rBlue:
+                mFontColorRef.setValue("blue");
+                break;
+        }
+
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        if (dataSnapshot.getValue(String.class) != null) {
+            String key = dataSnapshot.getKey();
+            if (key.equals("heading")) {
+                String heading = dataSnapshot.getValue(String.class);
+                headingText.setText(heading);
+            } else if (key.equals("fontcolor")) {
+                String color = dataSnapshot.getValue(String.class);
+
+                if (color.equals("red")) {
+                    headingText.setTextColor(ContextCompat.getColor(this, R.color.colorRed));
+                    rbRed.setChecked(true);
+                } else if (color.equals("blue")) {
+                    headingText.setTextColor(ContextCompat.getColor(this, R.color.colorBlue));
+                    rbBlue.setChecked(true);
+
+                }
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
 
     }
 }
